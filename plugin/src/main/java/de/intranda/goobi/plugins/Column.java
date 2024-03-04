0efe60e3e29609140d2be1ec12c6da6fbeb8ccc8
Robert Sehr <robert.sehr@intranda.com>
2024-03-04 13:34:00 +0100

    read configuration, get content from excel file

---
+++ plugin/src/main/java/de/intranda/goobi/plugins/Column.java
@@ -0,0 +1,15 @@
+package de.intranda.goobi.plugins;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Column {
+
+    // analyze header row
+    private String columnName;
+
+    private String metadataName;
+
+}
